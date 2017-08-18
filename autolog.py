5634542f3221b78a2e5b68e8f9bb0801aecde1b4
Kuilin Chen <alexchen199023@gmail.com>
2017-08-18 11:25:37 -0400

    Update

--- autolog.py
+++ autolog.py
@@ -1,27 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jul 10 13:17:31 2017
-
 @author: kchen
 """
-import time
+
 from bs4 import BeautifulSoup
 import io
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
-'''
-url = 'https://thanksaton.achievers.com/login'
-payload = {
-    'action': 'login',
-    'username': '43958',
-    'password': '112358'
-}
-
-r = requests.get(url)
-print(r.content)
-'''
+start_date = '2017-08-06'
+end_date = '2017-08-17'
 
 def parse_html(html_file):
     parsed_html = BeautifulSoup(html_file, 'lxml')
@@ -30,7 +19,7 @@ def parse_html(html_file):
     timeHour = parsed_html.body.find_all('div', attrs={'class':'timeHour'})
     roomAstudent = parsed_html.body.find_all('ul', attrs={'class':'roomAstudent'})
     n = len(timeMonth)
-    csv_file = io.open('new_courses.csv','w',encoding = 'utf-8-sig')
+    csv_file = io.open(teacher + '_new_courses.csv','w',encoding = 'utf-8-sig')
     for i in range(0, n):
         csv_file.write(timeMonth[i].text)
         csv_file.write(',')
@@ -42,48 +31,33 @@ def parse_html(html_file):
     
     csv_file.close()
 
-if __name__ == "__xxx__":
-    html_file = io.open('sample.html', 'r', encoding = 'utf-8').read()
-    parsed_html = BeautifulSoup(html_file, 'lxml')
-    teacher = parsed_html.body.find('h3', attrs={'id':'teachername'}).text
-    timeMonth = parsed_html.body.find_all('div', attrs={'class':'timeMonth'})
-    timeHour = parsed_html.body.find_all('div', attrs={'class':'timeHour'})
-    roomAstudent = parsed_html.body.find_all('ul', attrs={'class':'roomAstudent'})
-    numStudent = roomAstudent[0].findAll('li')
-    n = len(timeMonth)
-    csv_file = io.open('new_courses.csv','w',encoding = 'utf-8-sig')
-    for i in range(0, n):
-        csv_file.write(timeMonth[i].text)
-        csv_file.write(',')
-        csv_file.write(',')
-        csv_file.write(timeHour[i].text)
-        csv_file.write(',')
-        csv_file.write(roomAstudent[i].findAll('li')[1].text[0])
-        csv_file.write('\n')
-    
-    csv_file.close()
-
-if __name__ == "__main__":
-    # create a new Firefox session
-    browser = webdriver.Chrome()
-    browser.maximize_window()
-     
+def web_scrap(name, pin, start_date, end_date, browser):
     # navigate to the application home page
     browser.get("http://t.webi.com.cn/logon")
     username = browser.find_element_by_id("username")
     password = browser.find_element_by_id("password")
 
-    username.send_keys("alana1.801yzx")
-    password.send_keys("Password01!")
+    username.send_keys(name)
+    password.send_keys(pin)
     
     browser.find_element_by_class_name("loginButton").click()
     browser.get("http://t.webi.com.cn/course/list/0")
     
-    start_date = browser.find_element_by_id('from')
-    end_date = browser.find_element_by_id('to')
-    start_date.send_keys('2017-08-06')
-    end_date.send_keys('2017-08-17')
     
-    browser.find_element_by_class_name('listContSearch').submit()
+    browser.get('http://t.webi.com.cn/course/list/3?startDate=' + start_date + '&endDate=' + end_date)
     html_source = browser.page_source
     parse_html(html_source)
+    #logout
+    browser.get("http://t.webi.com.cn/home/logout")
+
+if __name__ == "__main__":
+    # create a new Chrome session
+    browser = webdriver.Chrome()
+    
+    with open('login_credentials.txt') as f:
+        for line in f:
+            [teacher, name, pin] = line.split()
+            web_scrap(name, pin, start_date, end_date, browser)
+            
+    browser.close()
+    
